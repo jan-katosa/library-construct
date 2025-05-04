843faeb5098aaa9e574433c11e31e1e9a21d6116
jan-katosa <sipka040708@gmail.com>
2025-05-04 23:59:03 +0200

    Lista, json kezelés kezdete

--- app/app.py
+++ app/app.py
@@ -1,19 +1,33 @@
+from os.path import join
 from tkinter import *
-from tkinter import ttk
+import json
+import commands
 
 root = Tk()
 root.title("Library Construct")
 
-main_frame = ttk.Frame(root, padding=5)
+main_frame = Frame(root)
 main_frame.grid()
 
 
 # Message on the top of the application window
-ttk.Label(main_frame, text="Welcome to Library Construct!").grid(column=0, row=0)
-ttk.Label(main_frame, text="Choose a language profile:").grid(column=0, row=1)
+Label(main_frame, text="Welcome to Library Construct!").grid(column=0, columnspan=2, row=0, padx=5)
+Label(main_frame, text="Choose a language profile:").grid(column=0, columnspan=2, row=1, padx=5)
 
-# TODO: Profile table/list
-ttk.Label(main_frame, text="Profile 1").grid(column=0, row=2)
+profile_listbox = Listbox(root)
+profile_listbox.grid(column=0, columnspan=2, row=2, rowspan=2, pady=15, padx=10)
 
+with open(join("data", "profiles.json"), 'r') as file:
+    data = json.load(file)
+
+for entry in data:
+    profile_listbox.insert(END, entry)
+
+choose_button = Button(root, text="Choose", command=commands.choose_profile)
+choose_button.grid(column=0, row=4, pady=5)
+delete_button = Button(root, text="Delete", command=commands.delete_profile)
+delete_button.grid(column=1, row=4, pady=5)
+quit_button = Button(root, text="Quit", command=quit)
+quit_button.grid(column=0, row=5, pady=5)
 
 root.mainloop()
---
+++ app/commands.py
@@ -0,0 +1,6 @@
+def choose_profile():
+    pass
+
+
+def delete_profile():
+    pass
